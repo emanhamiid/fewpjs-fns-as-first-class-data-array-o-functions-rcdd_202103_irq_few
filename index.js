375478f320cca47ef96f5158c9fe10c35cff18b7
eman hamid <emanhharki@gmail.com>
2021-05-07 17:04:36 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,22 @@
+
+function wakeDog(dogName,dogBree){
+ console.log(`Wake ${dogName} the ${dogBreed}`);
+}
+function leashDog(dogName,dogBreed){
+
+}
+function walkToPark(dogName,dogBreed){
+
+}
+function throwFrisbee(dogName,dogBreed){
+
+}
+function walkHome(dogName,dogBreed){
+
+}
+function unleashDog(dogName,dogBreed){
+
+}
+wakeDog("heh","hah")
+
+let routine = [];
